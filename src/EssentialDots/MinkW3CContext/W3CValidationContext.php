c7b27f0d5b65eba656be4ce26add3c6eab119762
Nikola Stojiljkovic <nikola.stojiljkovic@essentialdots.com>
2015-08-07 11:24:06 +0200

    [TASK] Migrate complete logic to W3C Validator JSON API
    Related: #1923

--- src/EssentialDots/MinkW3CContext/W3CValidationContext.php
+++ src/EssentialDots/MinkW3CContext/W3CValidationContext.php
@@ -2,20 +2,13 @@
 
 namespace EssentialDots\MinkW3CContext;
 
-use Behat\Behat\Context\ClosuredContextInterface,
-	Behat\Behat\Context\TranslatedContextInterface,
-	Behat\Behat\Context\BehatContext,
-	Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-	Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\ClosuredContextInterface, Behat\Behat\Context\TranslatedContextInterface, Behat\Behat\Context\BehatContext, Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode, Behat\Gherkin\Node\TableNode;
 
+use Behat\Mink\Element\NodeElement;
 use Behat\MinkExtension\Context\MinkContext;
 
-use Behat\Mink\Exception\ElementNotFoundException,
-	Behat\Mink\Exception\ExpectationException,
-	Behat\Mink\Exception\ResponseTextException,
-	Behat\Mink\Exception\ElementHtmlException,
-	Behat\Mink\Exception\ElementTextException;
+use Behat\Mink\Exception\ElementNotFoundException, Behat\Mink\Exception\ExpectationException, Behat\Mink\Exception\ResponseTextException, Behat\Mink\Exception\ElementHtmlException, Behat\Mink\Exception\ElementTextException;
 
 use \Guzzle\Http\Exception\CurlException;
 
@@ -23,6 +16,11 @@ use \Guzzle\Http\Exception\CurlException;
  * Features context.
  */
 class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
+
+	/**
+	 * @var string
+	 */
+	protected $url = 'https://validator.w3.org/nu/?out=json';
 
 	/**
 	 * @var array;
@@ -35,27 +33,6 @@ class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
 	protected $errors;
 
 	/**
-	 * @throws \Behat\Behat\Exception\PendingException
-	 */
-	protected function initializeSettings(\Behat\Mink\Session $session) {
-		$this->isInitialized = TRUE;
-
-		// initialize settings
-		// load general settings
-		$generalSettingsFile = __DIR__ . DIRECTORY_SEPARATOR . 'Settings' . DIRECTORY_SEPARATOR . 'settings.general.ini';
-		if (@file_exists($generalSettingsFile) && @is_file($generalSettingsFile)) {
-			$generalSettings = parse_ini_file($generalSettingsFile, true);
-			if ($generalSettings === FALSE) {
-				throw new \Exception("Wrong format of the settings.general.ini file.");
-			}
-		} else {
-			$generalSettings = array();
-		}
-
-		$this->settings = $generalSettings;
-	}
-
-	/**
 	 * @When /^I check source code on W3C validation service$/
 	 */
 	public function iCheckSourceCodeOnW3CValidationService() {
@@ -66,52 +43,7 @@ class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
 		// get compressed source code of page
 		$compressedSourceCode = $this->compressMarkup($this->getSession()->getPage()->getContent());
 
-		$curlException = null;
-		do {
-			try {
-				$this->getSession()->visit("http://validator.w3.org/#validate_by_input");
-				$curlException = null;
-			} catch (CurlException $excp) {
-				$curlException = $excp;
-			}
-		} while ($curlException != null);
-
-		// set source code which will be tested
-
-		$inputBox = $this->getSession()->getPage()->find('css', $this->settings['selectors']['sourceTextArea']);
-		/** @var $inputBox NodeElement */
-		if (!$inputBox) {
-			throw new ExpectationException("Cannot find input box in W3C website. W3C validator might be unavailable or changed the markup...", $this->getSession());
-		}
-		$inputBox->setValue($compressedSourceCode);
-
-		// press Check button
-		$submitButton = $this->getSession()->getPage()->find('css', $this->settings['selectors']['submitButton']);
-		/** @var $submitButton NodeElement */
-		if (!$submitButton) {
-			throw new ExpectationException("Cannot find submit box in W3C website. W3C validator might be unavailable or changed the markup...", $this->getSession());
-		}
-
-		do {
-			try {
-				$submitButton->press();
-				$curlException = null;
-			} catch (CurlException $excp) {
-				$curlException = $excp;
-			}
-		} while ($curlException != null);
-
-		// find warnings
-		$warnings = $this->getSession()->getPage()->findAll('css', $this->settings['selectors']['warning']);
-		foreach ($warnings as $warn) {
-			$this->warnings[] = $warn->getText();
-		}
-
-		// find errors
-		$errors = $this->getSession()->getPage()->findAll('css', $this->settings['selectors']['errors']);
-		foreach ($errors as $err) {
-			$this->errors[] = $err->getText();
-		}
+		$this->validate($compressedSourceCode);
 	}
 
 	/**
@@ -121,7 +53,7 @@ class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
 		$expectedErrorCount = $this->getNumber($countStr);
 		$actualErrorCount = count($this->errors);
 		if ($actualErrorCount != $expectedErrorCount) {
-			throw new ExpectationException("Expected errors: {$expectedErrorCount}. Actual found errors: {$actualErrorCount}." . ($actualErrorCount ? (" Detailed list of errors: \n" . implode("\n", $this->errors)) : ''), $this->getSession());
+			throw new ExpectationException("Expected errors: {$expectedErrorCount}. Actual found errors: {$actualErrorCount}." . ($actualErrorCount ? (" Detailed list of errors: \n" . implode("\n---------------------------------------------------------\n", $this->errors)) : ''), $this->getSession());
 		}
 	}
 
@@ -132,7 +64,7 @@ class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
 		$expectedWarningCount = $this->getNumber($countStr);
 		$actualWarningCount = count($this->errors);
 		if ($actualWarningCount != $expectedWarningCount) {
-			throw new ExpectationException("Expected warnings: {$expectedWarningCount}. Actual found warnings: {$actualWarningCount}." . ($actualWarningCount ? (" Detailed list of warnings: \n" . implode("\n", $this->warnings)) : ''), $this->getSession());
+			throw new ExpectationException("Expected warnings: {$expectedWarningCount}. Actual found warnings: {$actualWarningCount}." . ($actualWarningCount ? (" Detailed list of warnings: \n" . implode("\n---------------------------------------------------------\n", $this->warnings)) : ''), $this->getSession());
 		}
 	}
 
@@ -177,5 +109,51 @@ class W3CValidationContext extends \EssentialDots\Weasel\GeneralRawMinkContext {
 	 */
 	protected function resetErrors() {
 		$this->errors = array();
+	}
+
+	/**
+	 * External call to the W3C Validation API, using curl.
+	 *
+	 * @param $html
+	 * @throws ExpectationException
+	 */
+	protected function validate($html) {
+		$resource = curl_init($this->url);
+		curl_setopt($resource, CURLOPT_USERAGENT, 'curl');
+		curl_setopt($resource, CURLOPT_POST, true);
+		curl_setopt($resource, CURLOPT_HTTPHEADER, array('Content-Type: text/html; charset=utf-8'));
+		curl_setopt($resource, CURLOPT_POSTFIELDS, $html);
+		curl_setopt($resource, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+		$response = json_decode(curl_exec($resource), TRUE);
+		if ($response === NULL || !is_array($response)) {
+			throw new ExpectationException('Could not parse W3C JSON API output', $this->getSession());
+		}
+		if (array_key_exists('messages', $response)) {
+			foreach ($response['messages'] as $message) {
+				switch ($message['type']) {
+					case 'info':
+						if (array_key_exists('subType', $message) && $message['subType'] === 'warning') {
+							$this->warnings[] = "
+message: {$message['message']}
+line: {$message['lastLine']}
+column: {$message['lastColumn']}
+extract: {$message['extract']}
+";
+						}
+						break;
+					case 'error':
+						$this->errors[] = "
+message: {$message['message']}
+line: {$message['lastLine']}
+column: {$message['lastColumn']}
+extract: {$message['extract']}
+";
+						break;
+					default:
+				}
+
+			}
+		}
 	}
 }
